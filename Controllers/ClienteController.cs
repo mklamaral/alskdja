e7cc1660f5edb28523bcf297b0501d07c257217c
Mikael Amaral <mikael.a.amaral07@gmail.com>
2024-08-12 14:25:39 -0300

    adicionando a view 'BuscaCliente' e atualizando o codigo 'ClienteController' para funcionar com a nova view

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -1,12 +1,69 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AulaDeASPNet.Data;
+using AulaDeASPNet.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AulaDeASPNet.Controllers
 {
     public class ClienteController : Controller
     {
-        public IActionResult CadastroCliente()
+        private readonly AulaContext _context;
+
+        public ClienteController(AulaContext context)
         {
-            return View();
+            _context = context;
+        }
+        //Buscar Clientes
+        public async Task<IActionResult> BuscaCliente()
+        {
+            return View(await _context.Clientes.ToListAsync());
+        }
+
+        //Detalhes Clientes
+        public async Task<IActionResult> DetalhesCliente(int Id)
+        {
+            return View(await _context.Clientes.FindAsync(Id));
+        }
+
+        //Alterar Clientes
+        public async Task<IActionResult> AlterarCliente(int Id)
+        {
+            return View(await _context.Clientes.FindAsync(Id));
+        }
+
+        // Cadastro de Clientes
+        public async Task<IActionResult> CadastroCliente(int? Id)
+        {
+            if (Id == null)
+            {
+                return View();
+            }
+            else
+            {
+                return View(await _context.Clientes.FindAsync(Id));
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CadastroCliente([Bind("Id,Nome,RG,CPF,Usuario,Senha,CEP,UF,Cidade,Bairro,Rua,Numero,Complemento")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                if (cliente.Id != 0)
+                {
+                    _context.Update(cliente);
+                    await _context.SaveChangesAsync();
+                }
+                else
+                {
+                    _context.Add(cliente);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("BuscaCliente");
+                }
+                return RedirectToAction("BuscaCliente");
+            }
+            return View(cliente);
         }
     }
 }
--- Data/AulaContext.cs
+++ Data/AulaContext.cs
@@ -5,8 +5,9 @@ namespace AulaDeASPNet.Data
     public class AulaContext : DbContext
     {
         //public AulaContext(DbContextOptions options) : base(options)
-    public AulaContext(DbContextOptions<AulaContext> options) : base(options)
+        public AulaContext(DbContextOptions<AulaContext> options) : base(options)
         {
         }
+        public DbSet<AulaDeASPNet.Models.Cliente> Clientes { get; set; } = default!;
     }
 }
